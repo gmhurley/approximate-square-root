e1b4dab04c15934720d7f81cc9399aae9f8d59f3
Glenn Hurley <glenn.hurley@gmail.com>
2015-09-01 14:47:08 -0400

    Error messages and iteration counting.

--- square_root.py
+++ square_root.py
@@ -6,16 +6,21 @@ minus, when you square your calculated square root and compare it to
 your original number.)
 """
 
+rounds = 1
+
 
 def ask_for_number():
     """Asks user for a positive number"""
-    number = 0
+    number = None
     while True:
         if number > 0:
             return number
+        elif number is not None:
+            print("Let's try to be positive this time. {} is just so negative.".format(number))
         try:
             number = int(input("Please provide a positive nuumber: "))
         except:
+            print("Silly human, that's not a number, let alone a positive one!")
             pass
 
 
@@ -30,6 +35,9 @@ def newtons_method(num, guess=None):
     if new_guess == guess:
         print("The square root of {} is {}.".format(num, guess))
     else:
+        global rounds
+        print("Iteration # {}".format(rounds))
+        rounds += 1
         newtons_method(num, new_guess)
 
 newtons_method(num=ask_for_number())
